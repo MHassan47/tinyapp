4dffd3e9195d47969703d7d36cb214c7e1613223
Mohamed <mhassankd35@gmail.com>
2022-05-17 19:01:26 -0500

    added delete shortURL functionality

--- express_server.js
+++ express_server.js
@@ -46,9 +46,16 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  // const newPath = req.originalUrl.split("/:shortURL/delete")[1];
+  // res.redirect(newPath);
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL);
+
   res.redirect(longURL);
 });
 
